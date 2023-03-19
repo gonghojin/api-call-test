4b92bb37676f6634b1108219ba4a6be0d4637e17
gonghojin <yolip51@naver.com>
2023-03-19 18:10:33 +0900

    Initial commit

---
+++ src/test/java/com/gongdel/blog/KakaoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gongdel.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
